884b36800bd2c667fd0d68d8160b2c9d46ce6873
BenRai1 <benrainow@gmx.de>
2023-01-31 16:28:20 +0100

    Options for prompt added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react"
 import { Configuration, OpenAIApi } from "openai"
+import { Option } from "../components/Option"
+// import ipfs from "./ipfs"
 
 import "./App.css"
+import { getWrappedNative } from "@web3uikit/web3"
 
 const key = import.meta.env.VITE_OPENAI_API_KEY
 
@@ -24,40 +27,96 @@ function App() {
         "https://i.seadn.io/gcs/files/218f18093f45e60231106fb97221d63c.png?auto=format&w=1000"
     )
     const [chosenImageURL, setChosenImageUrl] = useState("")
-    const [choice, setChoice] = useState("")
+    const [choicePicture, setChoicePicture] = useState("")
+    let optionsArray = [
+        { name: "small", clicked: false },
+        { name: "red", clicked: false },
+        { name: "hat", clicked: false },
+        { name: "monocal", clicked: false },
+        { name: "green eyes", clicked: false },
+        { name: "confused", clicked: false },
+    ]
 
     const alt =
         "https://t3.ftcdn.net/jpg/01/91/95/30/360_F_191953033_gehQATeDoh5z6PyRDbeKyBZuS83CjMEF.jpg"
 
     const generateImage = async () => {
-        // const response = await openai.createImage({
-        //     prompt: "a white siamese cat",
-        //     n: 1,
-        //     size: "1024x1024",
-        // })
-        // console.log(response)
-        // setImageURL(response.data.data[0].url)
+        let prompt = "A bird with"
+        for (let i = 0; i < optionsArray.length; i++) {
+            if (optionsArray[i].clicked == true) {
+                prompt = prompt + " " + optionsArray[i].name
+            }
+        }
 
-        console.log(promt)
+        const stringPrompt = prompt.toString()
+
+        console.log(typeof stringPrompt)
+
+        const response = await openai.createImage({
+            prompt: stringPrompt,
+            n: 4,
+            size: "1024x1024",
+            // response_format: "b64_json",
+        })
+        console.log(response)
+        setImageURL1(response.data.data[0].url)
+        setImageURL2(response.data.data[1].url)
+        setImageURL3(response.data.data[2].url)
+        setImageURL4(response.data.data[3].url)
+
+        console.log(stringPrompt)
     }
 
     const setChosenImage = (chosenImageURL, imageId) => {
         setChosenImageUrl(chosenImageURL)
         console.log(chosenImageURL)
-        setChoice(imageId)
-        console.log(choice)
+        setChoicePicture(imageId)
+        console.log(choicePicture)
+    }
+
+    const mint = () => {
+        const image = document.getElementById("image1")
+
+        // Get the remote image as a Blob with the fetch API
+        fetch(image.src)
+            .then((res) => res.blob())
+            .then((blob) => {
+                // Read the Blob as DataURL using the FileReader API
+                const reader = new FileReader()
+                reader.onloadend = () => {
+                    console.log(reader.result)
+                    // Logs data:image/jpeg;base64,wL2dvYWwgbW9yZ...
+
+                    // Convert to Base64 string
+                    const base64 = getBase64StringFromDataURL(reader.result)
+                    console.log(base64)
+                    // Logs wL2dvYWwgbW9yZ...
+                }
+                reader.readAsDataURL(blob)
+            })
     }
 
     return (
         <div className="App">
             <h1>Final project</h1>
+            <h2>Chose up to 4 attributes</h2>
+            <div className="checkboxesContainer">
+                {optionsArray.map((op, index) => {
+                    return (
+                        <div key={index} className="checkbox">
+                            <Option name={op.name} index={index} optionsArray={optionsArray} />
+                        </div>
+                    )
+                })}
+            </div>
             <div className="card">
                 <button onClick={generateImage}>Generate Image</button>
             </div>
             <div className="suggestionsContainer">
                 <div className="suggestionContainer">
                     <img
-                        className={choice === "1" ? "imageChosen" : "img"}
+                        id="image1"
+                        className={choicePicture === "1" ? "imageChosen" : "img"}
                         src={imageUrl1}
                         alt={alt}
                     />
@@ -65,7 +124,7 @@ function App() {
                 </div>
                 <div className="suggestionContainer">
                     <img
-                        className={choice == "2" ? "imageChosen" : "img"}
+                        className={choicePicture == "2" ? "imageChosen" : "img"}
                         src={imageUrl2}
                         alt={alt}
                     />
@@ -73,7 +132,7 @@ function App() {
                 </div>
                 <div className="suggestionContainer">
                     <img
-                        className={choice == "3" ? "imageChosen" : "img"}
+                        className={choicePicture == "3" ? "imageChosen" : "img"}
                         src={imageUrl3}
                         alt={alt}
                     />
@@ -81,13 +140,14 @@ function App() {
                 </div>
                 <div className="suggestionContainer">
                     <img
-                        className={choice == "4" ? "imageChosen" : "img"}
+                        className={choicePicture == "4" ? "imageChosen" : "img"}
                         src={imageUrl4}
                         alt={alt}
                     />
                     <button onClick={() => setChosenImage(imageUrl4, "4")}>Choose Nr.4</button>
                 </div>
             </div>
+            <button onClick={() => mint()}>Mint Choice</button>
         </div>
     )
 }
