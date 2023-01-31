aa28a0e27175a9933f506beefa3597c10127a40f
BenRai1 <benrainow@gmx.de>
2023-01-31 10:54:01 +0100

    Dalle API working

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,45 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useState } from "react"
+import { Configuration, OpenAIApi } from "openai"
+
+import "./App.css"
+
+const key = import.meta.env.VITE_OPENAI_API_KEY
+
+const configuration = new Configuration({
+    apiKey: key,
+})
+const openai = new OpenAIApi(configuration)
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [imageUrl, setImageURL] = useState("")
 
-  return (
-    <div className="App">
-      <h1>Final project</h1>
-      <div className="card">
-        <button>Generate Image</button>
-      </div>
-    </div>
-  );
+    const generateImage = async () => {
+        // const response = await openai.createCompletion({
+        //     model: "text-davinci-003",
+        //     prompt: "Say this is a test",
+        //     temperature: 0,
+        //     max_tokens: 2,
+        // })
+        // console.log(response)
+
+        const response = await openai.createImage({
+            prompt: "a white siamese cat",
+            n: 1,
+            size: "1024x1024",
+        })
+        console.log(response)
+        setImageURL(response.data.data[0].url)
+    }
+
+    return (
+        <div className="App">
+            <h1>Final project</h1>
+            <div className="card">
+                <button onClick={generateImage}>Generate Image</button>
+            </div>
+            <img src={imageUrl} alt="no Image" />
+        </div>
+    )
 }
 
-export default App;
+export default App
