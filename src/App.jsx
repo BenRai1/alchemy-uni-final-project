3abb07559f1525bd07a291a5ea608340078c984a
BenRai1 <benrainow@gmx.de>
2023-02-02 09:47:51 +0100

    Using base64

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Option } from "../components/Option"
 // import ipfs from "./ipfs"
 
 import "./App.css"
-import { getWrappedNative } from "@web3uikit/web3"
 
 const key = import.meta.env.VITE_OPENAI_API_KEY
 
@@ -14,34 +13,34 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 function App() {
-    const [imageUrl1, setImageURL1] = useState(
+    const [base64_1, setbase64_1] = useState(
         "https://i.seadn.io/gcs/files/4f82aecc2591d4f79e18cabb34c620b6.png?auto=format&w=1000"
     )
-    const [imageUrl2, setImageURL2] = useState(
+    const [base64_2, setbase64_2] = useState(
         "https://i.seadn.io/gcs/files/1089ca3f2fe5e9c4f32537077ee8feb0.png?auto=format&w=1000"
     )
-    const [imageUrl3, setImageURL3] = useState(
+    const [base64_3, setbase64_3] = useState(
         "https://i.seadn.io/gcs/files/45aefae563f35937711b6a95f70110e6.png?auto=format&w=1000"
     )
-    const [imageUrl4, setImageURL4] = useState(
+    const [base64_4, setbase64_4] = useState(
         "https://i.seadn.io/gcs/files/218f18093f45e60231106fb97221d63c.png?auto=format&w=1000"
     )
     const [chosenImageURL, setChosenImageUrl] = useState("")
     const [choicePicture, setChoicePicture] = useState("")
     let optionsArray = [
         { name: "small", clicked: false },
-        { name: "red", clicked: false },
-        { name: "hat", clicked: false },
+        { name: "black", clicked: false },
+        { name: "scarf", clicked: false },
         { name: "monocal", clicked: false },
-        { name: "green eyes", clicked: false },
-        { name: "confused", clicked: false },
+        { name: "green cloud", clicked: false },
+        { name: "happy", clicked: false },
     ]
 
     const alt =
         "https://t3.ftcdn.net/jpg/01/91/95/30/360_F_191953033_gehQATeDoh5z6PyRDbeKyBZuS83CjMEF.jpg"
 
     const generateImage = async () => {
-        let prompt = "A bird with"
+        let prompt = "A dog with"
         for (let i = 0; i < optionsArray.length; i++) {
             if (optionsArray[i].clicked == true) {
                 prompt = prompt + " " + optionsArray[i].name
@@ -56,13 +55,20 @@ function App() {
             prompt: stringPrompt,
             n: 4,
             size: "1024x1024",
-            // response_format: "b64_json",
+            response_format: "b64_json",
         })
         console.log(response)
-        setImageURL1(response.data.data[0].url)
-        setImageURL2(response.data.data[1].url)
-        setImageURL3(response.data.data[2].url)
-        setImageURL4(response.data.data[3].url)
+
+        const link1 = "data:image/png;base64," + response.data.data[0].b64_json
+        const link2 = "data:image/png;base64," + response.data.data[1].b64_json
+        const link3 = "data:image/png;base64," + response.data.data[2].b64_json
+        const link4 = "data:image/png;base64," + response.data.data[3].b64_json
+        console.log(link1)
+
+        setbase64_1(link1)
+        setbase64_2(link2)
+        setbase64_3(link3)
+        setbase64_4(link4)
 
         console.log(stringPrompt)
     }
@@ -117,34 +123,34 @@ function App() {
                     <img
                         id="image1"
                         className={choicePicture === "1" ? "imageChosen" : "img"}
-                        src={imageUrl1}
+                        src={base64_1}
                         alt={alt}
                     />
-                    <button onClick={() => setChosenImage(imageUrl1, "1")}>Choose Nr.1</button>
+                    <button onClick={() => setChosenImage(base64_1, "1")}>Choose Nr.1</button>
                 </div>
                 <div className="suggestionContainer">
                     <img
                         className={choicePicture == "2" ? "imageChosen" : "img"}
-                        src={imageUrl2}
+                        src={base64_2}
                         alt={alt}
                     />
-                    <button onClick={() => setChosenImage(imageUrl2, "2")}>Choose Nr.2</button>
+                    <button onClick={() => setChosenImage(base64_2, "2")}>Choose Nr.2</button>
                 </div>
                 <div className="suggestionContainer">
                     <img
                         className={choicePicture == "3" ? "imageChosen" : "img"}
-                        src={imageUrl3}
+                        src={base64_3}
                         alt={alt}
                     />
-                    <button onClick={() => setChosenImage(imageUrl3, "3")}>Choose Nr.3</button>
+                    <button onClick={() => setChosenImage(base64_3, "3")}>Choose Nr.3</button>
                 </div>
                 <div className="suggestionContainer">
                     <img
                         className={choicePicture == "4" ? "imageChosen" : "img"}
-                        src={imageUrl4}
+                        src={base64_4}
                         alt={alt}
                     />
-                    <button onClick={() => setChosenImage(imageUrl4, "4")}>Choose Nr.4</button>
+                    <button onClick={() => setChosenImage(base64_4, "4")}>Choose Nr.4</button>
                 </div>
             </div>
             <button onClick={() => mint()}>Mint Choice</button>
